372271b4d46ad4c6ad7ec966915bf6a501c0bc32
jose <joseplk000@hotmail.com>
2018-10-05 01:51:02 -0300

    Projeto inicial
    Projeto contendo a ideia inicial do projeto android

---
+++ app/src/main/java/com/example/jose/vempravan/UserCadastro.kt
@@ -0,0 +1,22 @@
+package com.example.jose.vempravan
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_user_cadastro.*
+import kotlinx.android.synthetic.main.activity_user_login.*
+
+class UserCadastro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_cadastro)
+
+
+
+    }
+
+
+}
